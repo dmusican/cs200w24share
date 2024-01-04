97fa1590e20acc5940690d58429037cc2977b728
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-04 13:43:23 -0600

    Scripted auto-commit on change (2024-01-04 13:43:23) by gitwatch.sh

--- 2024-01-04/HaveFun.java
+++ 2024-01-04/HaveFun.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HaveFun {
     public static void main(String[] args) {
-        System.out.println("hey");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Tell me your name:");
+        String response = in.nextLine();
+        System.out.println("Your name is " + response);
     }
 }
