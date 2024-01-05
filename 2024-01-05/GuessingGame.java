be2bb0ed7598927b46cef7bb32a5dc184e04dbc1
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-05 13:16:38 -0600

    Scripted auto-commit on change (2024-01-05 13:16:38) by gitwatch.sh

--- 2024-01-05/GuessingGame.java
+++ 2024-01-05/GuessingGame.java
@@ -1,2 +1,9 @@
 // Guessing game where user can try to guess the mystery number
 
+public class GuessingGame
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hi");
+    }
+}
