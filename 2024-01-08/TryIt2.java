daa64a29906c1af751b716d6fcd5ea1ac32ce7a5
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-08 12:42:19 -0600

    Scripted auto-commit on change (2024-01-08 12:42:19) by gitwatch.sh

---
+++ 2024-01-08/TryIt2.java
@@ -0,0 +1,17 @@
+public class TryIt2
+{
+    //def calculateDecades(age):
+    public static double calculateDecades(int age)
+    {
+        double decades = age / 10.0;
+        return decades;
+    }
+
+    public static void main(String[] args)
+    {
+        double answer1 = calculateDecades(9);
+        double answer2 = calculateDecades(127);
+        System.out.println(answer1);
+        System.out.println(answer2);
+    }
+}
