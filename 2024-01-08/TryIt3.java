e5a06f5b63eb46c5c30ed3d9a3174e056c407f2a
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-08 12:52:22 -0600

    Scripted auto-commit on change (2024-01-08 12:52:22) by gitwatch.sh

--- 2024-01-08/TryIt3.java
+++ 2024-01-08/TryIt3.java
@@ -1,4 +1,15 @@
 public class TryIt3 {
+    public static int addItUp(int[] numbers)
+    {
+        int total = 0;
+        for (int index=0; index < numbers.length; index++)
+        {
+            total = total + numbers[index];
+        }
+        return total;
+    }
+
+
     public static void main(String[] args)
     {
         int[] stuff = {9, 2, 3, 5, 7};
