77afac5cf924f1f0aa77a995de7945af5577a954
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-09 13:50:06 -0600

    Scripted auto-commit on change (2024-01-09 13:50:06) by gitwatch.sh

--- 2024-01-09/Directory.java
+++ 2024-01-09/Directory.java
@@ -3,10 +3,15 @@ public class Directory {
         Student s1 = new Student();
         s1.name = "Dave";
         s1.age = 27;
+        
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+        s1.doBirthday();
+        System.out.println(s1.age);
 
         Student s2 = new Student();
         s2.name = "Thea";
         s2.age = 45;
-        
+
     }
 }
