788698b1bbb9b8b5a2009450dd8bf9f46267cb97
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-10 12:34:55 -0600

    Scripted auto-commit on change (2024-01-10 12:34:55) by gitwatch.sh

---
+++ 2024-01-10/Tally.java
@@ -0,0 +1,16 @@
+public class Tally {
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        c1.reset();
+        c1.click();
+        c1.click();
+        int current = c1.getValue();
+        System.out.println("c1 shows " + current);
+        c1.click();
+        Counter c2 = new Counter();
+        c2.click();
+        System.out.println("c1 shows " + c1.getValue());
+        System.out.println("c1 shows " + c2.getValue());
+        
+    }
+}
