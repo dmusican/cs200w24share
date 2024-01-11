d87b047ac5f6016042f234607cd29fe4055e41ed
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-11 13:53:42 -0600

    Scripted auto-commit on change (2024-01-11 13:53:42) by gitwatch.sh

--- 2024-01-11/Die.java
+++ 2024-01-11/Die.java
@@ -2,11 +2,13 @@ public class Die {
     // instance variables
     private int numSides;
     private int showing;
+    private static int numDiceMade = 0;
 
     // constructor
     public Die(int numSidesValue) {
         numSides = numSidesValue;
         showing = 1;
+        numDiceMade = numDiceMade + 1;
     }
 
     public int getShowing() {
@@ -16,6 +18,10 @@ public class Die {
     public void roll() {
         showing =
            (int)(Math.random()*numSides + 1);
+    }
+
+    public int getNumDiceMade() {
+        return numDiceMade;
     }
     
 }
