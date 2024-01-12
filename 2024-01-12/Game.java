93bca4b91fac31c6357e6794ffbae0f069a85ae5
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-12 13:40:47 -0600

    Scripted auto-commit on change (2024-01-12 13:40:47) by gitwatch.sh

--- 2024-01-12/Game.java
+++ 2024-01-12/Game.java
@@ -1,10 +1,11 @@
 public class Game {
     public static void main(String[] args) {
         double randThing = Math.random();
+        Circle or Rectangle s;
         if (randThing > 0.5) {
-            Circle s = new Circle(10);
+            s = new Circle(10);
         } else {
-            Rectangle s = new Rectangle(2, 3);
+            s = new Rectangle(2, 3);
         }
         System.out.println(s.area());
         System.out.println("again " + s.area());
