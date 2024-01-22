bb494e25fc0d0ef4e2bb742c8d94a12d55fb9773
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-22 12:57:30 -0600

    Scripted auto-commit on change (2024-01-22 12:57:30) by gitwatch.sh

--- 2024-01-22/CS200ArrayList.java
+++ 2024-01-22/CS200ArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CS200ArrayList<T> implements CS200List<T> {
     private T[] array;
     private int numberOfEntries;
@@ -13,11 +15,25 @@ public class CS200ArrayList<T> implements CS200List<T> {
     }
 
     public void addHead(T item) {
+        // I'm full
+        if (numberOfEntries == array.length) {
+            array = Arrays.copyOf(array, 2*array.length);
+
+        }
         for (int i=numberOfEntries; i >= 1; i--) {
             array[i] = array[i-1];
         }
         array[0] = item;
         numberOfEntries++;
+    }
+
+    public T getHead() {
+        if (numberOfEntries > 0) {
+            return array[0];
+        } else {
+            throw new RuntimeException(
+                "BAD BAD BAD list is empty");
+        }
     }
     
 }
