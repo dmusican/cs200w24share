e764d63807ad9e764bdc2ce8931ae93bd50344b3
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-31 13:05:54 -0600

    Scripted auto-commit on change (2024-01-31 13:05:54) by gitwatch.sh

--- 2024-01-31/Shape.java
+++ 2024-01-31/Shape.java
@@ -1,8 +1,8 @@
 public class Shape {
     private String color;
 
-    public Shape(String orange) {
-        color = orange;
+    public Shape(String airplane) {
+        color = airplane;
     }
 
     public String getColor() {
