22aa46f4138028f0929d1e2fadeebacf25c5e5e6
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-31 12:52:36 -0600

    Scripted auto-commit on change (2024-01-31 12:52:36) by gitwatch.sh

--- 2024-01-31/Rectangle.java
+++ 2024-01-31/Rectangle.java
@@ -1,6 +1,7 @@
 public class Rectangle {
     private double length;
     private double width;
+    private String color;
 
     public Rectangle(double lengthValue,
                      double widthValue) {
@@ -10,5 +11,9 @@ public class Rectangle {
 
     public double area() {
         return length * width;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
