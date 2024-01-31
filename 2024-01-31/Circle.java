0ef8080a334d2e9e9e09646563d76cd71fee1064
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-31 12:52:06 -0600

    Scripted auto-commit on change (2024-01-31 12:52:06) by gitwatch.sh

--- 2024-01-31/Circle.java
+++ 2024-01-31/Circle.java
@@ -1,5 +1,6 @@
 public class Circle {
     private double radius;
+    private String color;
 
     public Circle(double radiusValue) {
         radius = radiusVale;
@@ -7,5 +8,9 @@ public class Circle {
 
     public double area() {
         return Math.PI * radius * radius;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
