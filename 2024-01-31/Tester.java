8aaab41f2b73f62242583be1e165e3dda6db9925
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-31 12:58:47 -0600

    Scripted auto-commit on change (2024-01-31 12:58:47) by gitwatch.sh

--- 2024-01-31/Tester.java
+++ 2024-01-31/Tester.java
@@ -1,7 +1,7 @@
 public class Tester {
     public static void main(String[] args) {
         Circle c = new Circle(3.2);
-        Rectangle r = new Rectangle(9.3);
+        Rectangle r = new Rectangle(9.3, 2.1);
         System.out.println(c.getColor());
         System.out.println(c.area());
         System.out.println(r.getColor());
