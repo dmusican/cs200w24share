b66a700c9a23497ab55a4238c402b156de876cfb
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-01 13:53:01 -0600

    Scripted auto-commit on change (2024-02-01 13:53:01) by gitwatch.sh

---
+++ 2024-02-01/SortingFun.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+public class SortingFun {
+    public static boolean checkSorted(List<Integer> nums) {
+        if (nums.size() == 1) {
+            return true;
+        } else if (nums.size() == 2) {
+            return (nums.get(0) <= nums.get(1));
+        } else {
+            return
+                (nums.get(0) <= nums.get(1)) &&
+                 checkSorted(nums.subList(1, nums.size())));
+        }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(9, 2, 3, 7);
+        System.out.println(checkSorted(list));
+    }
+}
