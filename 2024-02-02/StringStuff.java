d29e5c43b580cbd4c7c174eedbd164cb6a44d573
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-02 13:16:36 -0600

    Scripted auto-commit on change (2024-02-02 13:16:36) by gitwatch.sh

---
+++ 2024-02-02/StringStuff.java
@@ -0,0 +1,13 @@
+class StringStuff {
+    public static String reverse(String s) {
+        if (s.length() == 0) {
+            return "";
+        } else {
+            return reverse(s.substring(1) + s.charAt(0));
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("coconut"));
+    }
+}
