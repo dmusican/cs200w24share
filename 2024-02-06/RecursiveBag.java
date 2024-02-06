c9b16265e305735d9e296320eac610c5b2c0aa55
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-06 13:31:50 -0600

    Scripted auto-commit on change (2024-02-06 13:31:50) by gitwatch.sh

---
+++ 2024-02-06/RecursiveBag.java
@@ -0,0 +1,16 @@
+public class RecursiveBag<T> {
+    private T first;
+    private RecursiveBag<T> rest;
+
+    public RecursiveBag() {
+        first = null;
+        rest = null;
+    }
+
+    public void add(T item) {
+        if (first == null) {
+            first = item;
+        } else if (rest == null) {
+            rest = new RecursiveBag<T>();
+            rest.add(item);
+        }
