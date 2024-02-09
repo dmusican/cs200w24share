32672cc08cae243ecae8031839ffc504afd702dc
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-09 13:18:21 -0600

    Scripted auto-commit on change (2024-02-09 13:18:21) by gitwatch.sh

---
+++ 2024-02-09/Example2.java
@@ -0,0 +1,12 @@
+// Less-than / greater-than extravaganza
+public class Example1 {
+  public static void main(String[] args) {
+    int x = 3;
+    int y = 5;
+    if (x < y) {
+      System.out.println("yes");
+    } else {
+      System.out.println("no");
+    }
+  }
+}
