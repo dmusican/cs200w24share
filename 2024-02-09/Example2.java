38887a6af50d01295ec3ece8e965bfc09d91fd1e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-09 13:20:28 -0600

    Scripted auto-commit on change (2024-02-09 13:20:28) by gitwatch.sh

--- 2024-02-09/Example2.java
+++ 2024-02-09/Example2.java
@@ -3,7 +3,7 @@ public class Example2 {
   public static void main(String[] args) {
     String x = "hi";
     String y = "bye";
-    System.out.println(x.comareTo(y));
+    System.out.println(x.compareTo(y));
     if (x.compareTo(y) < 0){
       System.out.println("yes");
     } else {
