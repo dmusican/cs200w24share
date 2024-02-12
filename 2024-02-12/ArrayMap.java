f60122e11fb73c5a95dd854acf09031c58735f86
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-12 12:45:49 -0600

    Scripted auto-commit on change (2024-02-12 12:45:49) by gitwatch.sh

---
+++ 2024-02-12/ArrayMap.java
@@ -0,0 +1,20 @@
+public class ArrayMap<K, V> {
+
+  public void put(K key, V value) {
+
+  }
+
+  public V get(K key) {
+
+  }
+
+
+  public static void main(String[] args) {
+
+    ArrayMap<String, Integer> myMap =
+      new ArrayMap<String, Integer>();
+    myMap.put('Dave', 82);
+    myMap.put('Quoc', 5);
+    System.out.println(myMap.get('Quoc'));
+  }
+}
