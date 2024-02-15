1427cba4c0184b87c76ef43294c64e98b3a64267
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-15 13:23:48 -0600

    Scripted auto-commit on change (2024-02-15 13:23:48) by gitwatch.sh

--- 2024-02-15/BinaryTree.java
+++ 2024-02-15/BinaryTree.java
@@ -1,4 +1,22 @@
 public class BinaryTree<T> {
-    private T key;    // actual data
-    private BinaryTree<T> leftChild;
-    private BinaryTree<T> rightChild;
+    public T key;    // actual data
+    public BinaryTree<T> leftChild;
+    public BinaryTree<T> rightChild;
+
+    public BinaryTree(T rootData) {
+        key = rootData;
+        leftChild = null;
+        rightChild = null;
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        BinaryTree<String> bt = new BinaryTree<String>("hello");
+        System.out.println(bt.rootData);
+        System.out.println(bt.leftChild);
+        System.out.println(bt.rightChild);
