064bcb0ffd1d0cd27a35933b1cfac7f1e2a9acc7
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-16 13:06:44 -0600

    Scripted auto-commit on change (2024-02-16 13:06:44) by gitwatch.sh

---
+++ 2024-02-16/BSTTryIt.java
@@ -0,0 +1,18 @@
+public class BSTTryIt {
+
+    public static void main(String[] args) {
+        BinarySearchTree<String, String> tree = new BinarySearchTree<>();
+
+        tree.put("France", "Paris");
+        tree.put("Japan", "Tokyo");
+        tree.put("Albania", "Tirana");
+        tree.put("Madagascar", "Antananarivo");
+        tree.put("Zimbabwe", "Harare");
+        tree.put("South Korea", "Seoul");
+        tree.put("Canada", "Ottawa");
+        System.out.println(tree); // show tree at its current state
+
+        System.out.println(tree.get("Japan"));  // "Tokyo"
+        System.out.println(tree.get("Brazil")); // null
+    }
+}
