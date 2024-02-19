16dbe4805c3f8c057b3c1354f79435239056a564
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-19 13:08:01 -0600

    Scripted auto-commit on change (2024-02-19 13:08:01) by gitwatch.sh

---
+++ 2024-02-16/Thing.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Thing<V> {
+    public ArrayList<V> makeOne(V item) {
+        ArrayList<V> alist = new ArrayList<V>();
+        alist.add(item);
+    }
+
+    public static void main(String[] args) {
+        Thing<String> ts = new Thing<String>();
+        ArrayList<String> result = ts.makeOne("hello");
+    }
+}
