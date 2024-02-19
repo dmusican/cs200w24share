15be5ba61f5cec0eafa2b373f3d9958177f20396
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-19 13:07:58 -0600

    Scripted auto-commit on change (2024-02-19 13:07:58) by gitwatch.sh

--- 2024-02-16/GenericAdd.java
+++ 2024-02-16/GenericAdd.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class GenericAdd<V> {
+public class Thing<V> {
     public ArrayList<V> makeOne(V item) {
         ArrayList<V> alist = new ArrayList<V>();
         alist.add(item);
